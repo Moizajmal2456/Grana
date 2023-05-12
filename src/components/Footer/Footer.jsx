0fef4035d1ad5681aa9c32b9f50e4966976720cc
Moizajmal2456 <moizajmal2456@gmail.com>
2023-05-12 10:09:20 +0500

    Set footer design

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,4 @@
-import style from "./styles.modules.scss";
+import style from "./styles.module.scss";
 import Instagram from "../../images/instagram.webp";
 import Linkedin from "../../images/linkedin.webp";
 import Twitter from "../../images/twitter.webp";
@@ -12,7 +12,7 @@ return(
         <div className={style.Link_wrapper}>
             <div className={style.About}>
              <ul>
-              <ol>About</ol>
+              <ol><b>About</b></ol>
               <ol>Our Story</ol>
               <ol>Our Blog</ol>
               <ol>Career</ol>
@@ -21,7 +21,7 @@ return(
             </div>
             <div className={style.Quick_Links}>
              <ul>
-              <ol>Quick Links</ol>
+              <ol><b>Quick Links</b></ol>
               <ol>Projects in Pakistan</ol>
               <ol>Projects in Karachi</ol>
               <ol>Projects in Islamabad</ol>
@@ -30,25 +30,27 @@ return(
             </div>
             <div className={style.ContactUs}>
              <ul>
-              <ol>Contact Us</ol>
+              <ol><b>Contact Us</b></ol>
               <ol>info@graana.com</ol>
               <ol>111-555-555</ol>
              </ul> 
             </div>
-            <div className={style.images}>
+            <div className={style.Follow}>
              <h3>Follow Us</h3> 
+             <div className={style.Images}>
              <img src={Instagram} alt="instgram" /> 
              <img src={Linkedin} alt="linkedin" />
              <img src={Twitter} alt="twitter" />
              <img src={Youtube} alt="youtube" />
              <img src={Facebook} alt="facebook" />         
+             </div>
             </div>
         </div>
      <div className={style.Terms}> 
         <p>Copyright 2023 Graana. All rights reserved</p>
         <p>Terms & Conditions | Privacy Policy | Desclaimer</p>
      </div>
-     <div className={style.Footer}>
+     <div className={style.footer}>
         <img src={FooterImg} alt="footer"/>
      </div>
     </div>
